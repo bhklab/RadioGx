31643b23bc6f1ba9de728103c0cafadeb5d5d338
markcfreeman <marky.freeman@gmail.com>
2017-07-21 17:09:08 -0400

    remove sanity checking from internal functions

--- R/linearQuadratic.R
+++ R/linearQuadratic.R
@@ -1,11 +1,4 @@
 .linearQuadratic <- function(D, pars, SF_as_log = TRUE) {
-  CoreGx:::.sanitizeInput(x = D,
-                          pars = pars,
-                          x_as_log = FALSE,
-                          y_as_log = SF_as_log,
-                          y_as_pct = FALSE,
-                          trunc = FALSE,
-                          verbose = FALSE)
   
   SF <- -(pars[[1]] * D + pars[[2]] * D ^ 2)
 
